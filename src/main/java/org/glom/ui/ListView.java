6ea9a96286b6dbe28f0a6ca4e42fc0ac8c706a46
Murray Cumming <murrayc@murrayc.com>
2015-09-02 11:30:46 +0200

    ListView: Move TableModel into a separate class.

--- src/main/java/org/glom/ui/ListView.java
+++ src/main/java/org/glom/ui/ListView.java
@@ -2,70 +2,14 @@ package org.glom.ui;
 
 import org.glom.libglom.Document;
 import org.glom.libglom.Logger;
-import org.glom.libglom.layout.LayoutGroup;
-import org.glom.libglom.layout.LayoutItem;
 
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by murrayc on 9/1/15.
  */
 public class ListView extends JFrame {
-
-    private static class TableModel extends AbstractTableModel {
-
-        private final Document document;
-        private final String tableName;
-        private final List<LayoutItem> layoutItems;
-
-        public TableModel(final Document document, final String tableName) {
-            this.document = document;
-            this.tableName = tableName;
-            this.layoutItems = buildLayoutItems();
-        }
-
-        private List<LayoutItem> buildLayoutItems() {
-            final List<LayoutItem> items = new ArrayList<>();
-            final List<LayoutGroup> listGroups = document.getDataLayoutGroups("list", this.tableName);
-            for(final LayoutGroup group : listGroups) {
-                for(final LayoutItem item : group.getItems()) {
-                    items.add(item);
-                }
-            }
-
-            return items;
-        }
-
-        @Override
-        public int getRowCount() {
-            return 0;
-        }
-
-        @Override
-        public int getColumnCount() {
-            return layoutItems.size();
-        }
-
-        @Override
-        public Object getValueAt(int rowIndex, int columnIndex) {
-            return null;
-        }
-
-        @Override
-        public String getColumnName(int columnIndex) {
-            System.out.println("in");
-            final LayoutItem item = layoutItems.get(columnIndex);
-            if (item == null) {
-                return null;
-            }
-
-            return item.getTitle("");
-        }
-    }
 
     ListView() {
         super();
---
+++ src/main/java/org/glom/ui/TableModel.java
@@ -0,0 +1,63 @@
+package org.glom.ui;
+
+import org.glom.libglom.Document;
+import org.glom.libglom.layout.LayoutGroup;
+import org.glom.libglom.layout.LayoutItem;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by murrayc on 9/2/15.
+ */
+public class TableModel extends AbstractTableModel {
+
+    private final Document document;
+    private final String tableName;
+    private final List<LayoutItem> layoutItems;
+
+    public TableModel(final Document document, final String tableName) {
+        this.document = document;
+        this.tableName = tableName;
+        this.layoutItems = buildLayoutItems();
+    }
+
+    private List<LayoutItem> buildLayoutItems() {
+        final List<LayoutItem> items = new ArrayList<>();
+        final List<LayoutGroup> listGroups = document.getDataLayoutGroups("list", this.tableName);
+        for(final LayoutGroup group : listGroups) {
+            for(final LayoutItem item : group.getItems()) {
+                items.add(item);
+            }
+        }
+
+        return items;
+    }
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return layoutItems.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        System.out.println("in");
+        final LayoutItem item = layoutItems.get(columnIndex);
+        if (item == null) {
+            return null;
+        }
+
+        return item.getTitle("");
+    }
+}
